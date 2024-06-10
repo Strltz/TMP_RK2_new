97d57e14c258540466bc8356fd88b8c015070b9a
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-10 20:31:39 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,1 +1,13 @@
+#include <circle.h>
+#include <composed_object.h>
+#include <graphical_object.h>
+#include <line.h>
+#include <point.h>
+#include <primitive.h>
+#include <triangle.h>
+#include <gtest/gtest.h>
+//include <gmock/gmock.h>
+#include <iostream>
+#include <sstream>
+
 
