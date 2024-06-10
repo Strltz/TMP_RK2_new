0d5f1ff3af597aa6c994f526bf7aee65903479c1
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-11 02:31:22 +0300

    Update2 test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,4 +10,35 @@
 #include <iostream>
 #include <sstream>
 
+#include "gtest/gtest.h"
+#include "composed_object.h"
 
+TEST(ComposedObjectTest, DefaultConstructor) {
+    ComposedObject obj;
+    
+    // Assert that the default constructor initializes the member variables correctly
+    EXPECT_EQ(obj.getX(), 0);
+    EXPECT_EQ(obj.getY(), 0);
+    EXPECT_EQ(obj.getZ(), 0);
+}
+
+TEST(ComposedObjectTest, ParameterizedConstructor) {
+    ComposedObject obj(1, 2, 3);
+    
+    // Assert that the parameterized constructor sets the member variables correctly
+    EXPECT_EQ(obj.getX(), 1);
+    EXPECT_EQ(obj.getY(), 2);
+    EXPECT_EQ(obj.getZ(), 3);
+}
+
+TEST(ComposedObjectTest, SettersAndGetters) {
+    ComposedObject obj;
+    
+    obj.setX(4);
+    obj.setY(5);
+    obj.setZ(6);
+    
+    EXPECT_EQ(obj.getX(), 4);
+    EXPECT_EQ(obj.getY(), 5);
+    EXPECT_EQ(obj.getZ(), 6);
+}
