b8c2aaa1f7b45649cb481c81ef626f5c791c04af
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-12 14:44:25 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -17,18 +17,18 @@ TEST(tests, test1) {
 }
 
 TEST(tests, test2) {
-    composed_object obj3();
-    composed_object obj4();
-    line obj5();
+    composed_object obj3;
+    composed_object obj4;
+    line obj5;
     obj3.add(&obj5);
     obj4.add(&obj5);
     EXPECT_EQ(obj3, obj4);
 }
 
 TEST(tests, test3) {
-    composed_object obj1();
-    triangle obj2();
-    line obj3();
+    composed_object obj1;
+    triangle obj2;
+    line obj3;
     int a = obj1.size_of_components_vec() + 2;
     obj1.add(&obj2);
     obj1.add(&obj3);
