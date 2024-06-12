f58498dd1abbdf52f439341d3d011d1a914fb5dc
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-12 15:53:46 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,7 +10,20 @@
 #include <iostream>
 #include <sstream>
 
+TEST(tests, test1) {
+    composed_object obj1;
+    composed_object obj2(obj1);
+    EXPECT_EQ(obj1, obj2);
+}
 
+TEST(tests, test2) {
+    composed_object obj3;
+    composed_object obj4;
+    line obj5;
+    obj3.add(obj5);
+    obj4.add(obj5);
+    EXPECT_EQ(obj3, obj4);
+}
 
 TEST(tests, test3) {
     composed_object obj1;
